521299fd3aabb91d6cfbf23d6b18c7dc7f9a04d7
bryanares <bryanares12@gmail.com>
2023-02-20 09:03:21 +0300

    create Room Database

--- app/src/main/java/com/brian/jotz/data/JotzRoomDatabase.kt
+++ app/src/main/java/com/brian/jotz/data/JotzRoomDatabase.kt
@@ -1,4 +1,32 @@
 package com.brian.jotz.data
 
-class JotzRoomDatabase {
+import android.content.Context
+import androidx.lifecycle.ViewModelProvider.NewInstanceFactory.Companion.instance
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [Jotz::class], version = 1, exportSchema = false)
+abstract class JotzRoomDatabase : RoomDatabase() {
+
+    //dao
+    abstract fun jotzDao(): JotzDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: JotzRoomDatabase? = null
+        fun getDatabase(context: Context): JotzRoomDatabase {
+            return INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    JotzRoomDatabase::class.java,
+                    "jotz_database"
+                )
+                    .fallbackToDestructiveMigration()
+                    .build()
+                INSTANCE = instance
+                return instance
+            }
+        }
+    }
 }
