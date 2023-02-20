f69bcaead199ef54f021380edb0d583620b444a2
bryanares <bryanares12@gmail.com>
2023-02-20 09:10:50 +0300

    bind jot item data for the recyclerview  and set up click handler

--- app/src/main/java/com/brian/jotz/JotzListAdapter.kt
+++ app/src/main/java/com/brian/jotz/JotzListAdapter.kt
@@ -1,4 +1,57 @@
 package com.brian.jotz
 
-class JotzListAdapter {
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.brian.jotz.data.Jotz
+import com.brian.jotz.databinding.JotListItemBinding
+
+class JotzListAdapter(private val onItemClicked: (Jotz) -> Unit) :
+    ListAdapter<Jotz, JotzListAdapter.JotzViewHolder>(DiffCallback) {
+
+    class JotzViewHolder(private var binding: JotListItemBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind(jotz: Jotz) {
+            binding.apply {
+                jotListTitle.text = jotz.jotzTitle.toString()
+                jotListDesc.text = jotz.jotzBody.toString()
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): JotzViewHolder {
+        return JotzViewHolder(
+            JotListItemBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false
+            )
+        )
+    }
+
+    override fun onBindViewHolder(holder: JotzViewHolder, position: Int) {
+
+        val current = getItem(position)
+        holder.itemView.setOnClickListener {
+            onItemClicked(current)
+        }
+        holder.bind(current)
+    }
+
+    companion object {
+        private val DiffCallback = object : DiffUtil.ItemCallback<Jotz>() {
+            override fun areItemsTheSame(oldItem: Jotz, newItem: Jotz): Boolean {
+                return oldItem == newItem
+            }
+
+            override fun areContentsTheSame(oldItem: Jotz, newItem: Jotz): Boolean {
+                return oldItem.jotzTitle == newItem.jotzTitle
+            }
+
+        }
+    }
+
+
 }
