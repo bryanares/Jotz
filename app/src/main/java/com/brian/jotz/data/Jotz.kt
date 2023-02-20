49c428ca0a582089fa444f9db6dc880ca136a28c
bryanares <bryanares12@gmail.com>
2023-02-20 09:01:37 +0300

    entity defining Jotz item

--- app/src/main/java/com/brian/jotz/data/Jotz.kt
+++ app/src/main/java/com/brian/jotz/data/Jotz.kt
@@ -1,4 +1,17 @@
 package com.brian.jotz.data
 
-class Jotz {
-}
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "jotz")
+data class Jotz (
+        @PrimaryKey(autoGenerate = true)
+        val id : Int = 0,
+
+        @ColumnInfo(name = "title")
+        val jotzTitle : String,
+
+        @ColumnInfo(name = "body")
+        val jotzBody : String
+        )
