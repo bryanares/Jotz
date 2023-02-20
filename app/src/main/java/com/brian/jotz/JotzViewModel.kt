4847823bf8ed7cf3714a28ab170b31e6f5a1f477
bryanares <bryanares12@gmail.com>
2023-02-20 09:12:20 +0300

    add methods to do database operations and update the UI

--- app/src/main/java/com/brian/jotz/JotzViewModel.kt
+++ app/src/main/java/com/brian/jotz/JotzViewModel.kt
@@ -1,4 +1,63 @@
 package com.brian.jotz
 
-class JotzViewModel {
+import androidx.lifecycle.*
+import com.brian.jotz.data.Jotz
+import com.brian.jotz.data.JotzDao
+import kotlinx.coroutines.launch
+
+class JotzViewModel(private val jotzDao: JotzDao) : ViewModel() {
+
+    val allJotz : LiveData<List<Jotz>> = jotzDao.getItems().asLiveData()
+
+    //call dao method to insert data to db
+    private fun insertJotz(jotz: Jotz) {
+        viewModelScope.launch {
+            jotzDao.insert(jotz)
+        }
+    }
+
+    //get new jotz details and define the item
+    private fun getNewJotzEntry(jotzTitle: String, jotzBody: String): Jotz {
+        return Jotz(
+            jotzTitle = jotzTitle.toString(),
+            jotzBody = jotzBody.toString()
+        )
+    }
+
+    //add the new jotz to db using the insert method
+    fun addNewJotz(jotzTitle: String, jotzBody: String){
+        val newJotz = getNewJotzEntry(jotzTitle, jotzBody)
+        insertJotz(newJotz)
+    }
+
+    //check if entry is valid(not blank)
+    fun isJotzEntryValid(jotzTitle: String, jotzBody: String) : Boolean{
+        if (jotzTitle.isBlank() || jotzBody.isBlank()){
+            return false
+        }
+        return true
+    }
+
+    //retrieve Jotz item using id
+    fun retrieveJotz(id: Int) : LiveData<Jotz>{
+        return  jotzDao.getItem(id).asLiveData()
+    }
+
+    //delete Jotz item using id
+    fun deleteJotz(jotz: Jotz) {
+        viewModelScope.launch {
+            jotzDao.delete(jotz)
+        }
+    }
+
+}
+
+class JotzViewModelFactory(private val jotzDao: JotzDao) : ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(JotzViewModel::class.java)) {
+            @Suppress("UNCHECKED_CAST")
+            return JotzViewModel(jotzDao) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
 }
