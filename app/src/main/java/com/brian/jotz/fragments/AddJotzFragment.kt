0d647560a1b49e67c84c8c9d2221c550deb25f42
bryanares <bryanares12@gmail.com>
2023-02-20 09:04:53 +0300

    get user input and add to database

--- app/src/main/java/com/brian/jotz/fragments/AddJotzFragment.kt
+++ app/src/main/java/com/brian/jotz/fragments/AddJotzFragment.kt
@@ -5,56 +5,62 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
+import androidx.navigation.fragment.findNavController
+import com.brian.jotz.JotzApplication
+import com.brian.jotz.JotzViewModel
+import com.brian.jotz.JotzViewModelFactory
 import com.brian.jotz.R
+import com.brian.jotz.data.Jotz
+import com.brian.jotz.databinding.FragmentAddJotzBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [AddJotzFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class AddJotzFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+
+    private var _binding: FragmentAddJotzBinding? = null
+    private val binding get() = _binding!!
+
+    private val viewModel: JotzViewModel by activityViewModels() {
+        JotzViewModelFactory((activity?.application as JotzApplication).database.jotzDao())
     }
+    lateinit var jotz: Jotz
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add_jotz, container, false)
+        _binding = FragmentAddJotzBinding.inflate(inflater, container, false)
+        val view = binding.root
+
+        return view
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment AddJotzFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            AddJotzFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    //check if entry is valid
+    private fun isJotzEntryValid(): Boolean {
+        return viewModel.isJotzEntryValid(
+            binding.addTitle.text.toString(),
+            binding.addBody.text.toString()
+        )
+    }
+
+    //add new jotz entry
+    private fun addNewJotz() {
+        if (isJotzEntryValid()){
+            viewModel.addNewJotz(
+                binding.addTitle.text.toString(),
+                binding.addBody.text.toString(),
+            )
+        }
+        val action = AddJotzFragmentDirections.actionAddJotzFragmentToJotzListFragment()
+        findNavController().navigate(action)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.addJotBtn.setOnClickListener {
+            addNewJotz()
+        }
     }
 }
