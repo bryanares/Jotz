d41e5505625f4b56b29d56fdf50f695c76f22fb6
bryanares <bryanares12@gmail.com>
2023-02-20 09:02:30 +0300

    interface with database methods

--- app/src/main/java/com/brian/jotz/data/JotzDao.kt
+++ app/src/main/java/com/brian/jotz/data/JotzDao.kt
@@ -1,4 +1,21 @@
 package com.brian.jotz.data
 
+import androidx.room.*
+import kotlinx.coroutines.flow.Flow
+
+@Dao
 interface JotzDao {
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insert(jotz: Jotz)
+
+    @Delete
+    suspend fun delete(jotz: Jotz)
+
+    //get specific item from id
+    @Query("SELECT * FROM jotz WHERE id = :id")
+    fun getItem(id : Int) : Flow<Jotz>
+    //get all items
+    @Query("SELECT * FROM jotz ORDER BY id DESC")
+    fun getItems(): Flow<List<Jotz>>
 }
