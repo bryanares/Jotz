8b31c9c54b452896408f20b4006f265a1ab653be
bryanares <bryanares12@gmail.com>
2023-04-27 11:10:04 +0300

    fix R related errors by removing R imports

--- app/src/main/java/com/brian/jotz/MainActivity.kt
+++ app/src/main/java/com/brian/jotz/MainActivity.kt
@@ -3,12 +3,28 @@ package com.brian.jotz
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatDelegate
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupActionBarWithNavController
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
+    private lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         //force light mode on physical testing device
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fragmentContainerView2) as NavHostFragment
+        navController = navHostFragment.navController
+
+        setupActionBarWithNavController(navController)
+    }
+
+    /**
+     * Handle navigation when the user chooses Up from the action bar.
+     */
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
