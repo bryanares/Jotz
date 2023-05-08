38daabc571ce38c6529dd2db7b49423fe2c14de8
bryanares <bryanares12@gmail.com>
2023-05-08 10:28:06 +0300

    edit jotz functionality

--- app/src/main/java/com/brian/jotz/fragments/AddJotzFragment.kt
+++ app/src/main/java/com/brian/jotz/fragments/AddJotzFragment.kt
@@ -1,16 +1,17 @@
 package com.brian.jotz.fragments
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
 import com.brian.jotz.JotzApplication
 import com.brian.jotz.JotzViewModel
 import com.brian.jotz.JotzViewModelFactory
-import com.brian.jotz.R
 import com.brian.jotz.data.Jotz
 import com.brian.jotz.databinding.FragmentAddJotzBinding
 
@@ -24,6 +25,7 @@ class AddJotzFragment : Fragment() {
         JotzViewModelFactory((activity?.application as JotzApplication).database.jotzDao())
     }
     lateinit var jotz: Jotz
+    private val navigationArgs: JotzDetailFragmentArgs by navArgs()
 
 
     override fun onCreateView(
@@ -47,7 +49,7 @@ class AddJotzFragment : Fragment() {
 
     //add new jotz entry
     private fun addNewJotz() {
-        if (isJotzEntryValid()){
+        if (isJotzEntryValid()) {
             viewModel.addNewJotz(
                 binding.addTitle.text.toString(),
                 binding.addBody.text.toString(),
@@ -57,10 +59,43 @@ class AddJotzFragment : Fragment() {
         findNavController().navigate(action)
     }
 
+    /** Edit Jotz Functionality below
+     * */
+
+    private fun bind(jotz: Jotz) {
+        binding.apply {
+            addTitle.setText(jotz.jotzTitle, TextView.BufferType.SPANNABLE)
+            addBody.setText(jotz.jotzBody, TextView.BufferType.SPANNABLE)
+            addJotBtn.setOnClickListener { updateJotz() }
+        }
+    }
+
+    private fun updateJotz() {
+        if (isJotzEntryValid()) {
+            viewModel.updateJotzEntry(
+                this.navigationArgs.jotId,
+                this.binding.addTitle.text.toString(),
+                this.binding.addBody.text.toString()
+            )
+
+            val action = AddJotzFragmentDirections.actionAddJotzFragmentToJotzListFragment()
+            findNavController().navigate(action)
+        }
+    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding.addJotBtn.setOnClickListener {
-            addNewJotz()
+        val id = navigationArgs.jotId
+
+        if (id > 0) {
+            viewModel.retrieveJotz(id).observe(this.viewLifecycleOwner) { selectedJotz ->
+                jotz = selectedJotz
+                bind(jotz)
+            }
+        } else {
+            binding.addJotBtn.setOnClickListener {
+                addNewJotz()
+            }
         }
     }
 }
