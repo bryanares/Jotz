ec02f8f83a07f8f9ff98f66435c27138aded32a4
bryanares <bryanares12@gmail.com>
2023-05-08 10:12:47 +0300

    update and delete jotz

--- app/src/main/java/com/brian/jotz/JotzViewModel.kt
+++ app/src/main/java/com/brian/jotz/JotzViewModel.kt
@@ -7,7 +7,7 @@ import kotlinx.coroutines.launch
 
 class JotzViewModel(private val jotzDao: JotzDao) : ViewModel() {
 
-    val allJotz : LiveData<List<Jotz>> = jotzDao.getItems().asLiveData()
+    val allJotz: LiveData<List<Jotz>> = jotzDao.getItems().asLiveData()
 
     //call dao method to insert data to db
     private fun insertJotz(jotz: Jotz) {
@@ -24,23 +24,42 @@ class JotzViewModel(private val jotzDao: JotzDao) : ViewModel() {
         )
     }
 
+    private fun updateJotz(jotz: Jotz) {
+        viewModelScope.launch {
+            jotzDao.update(jotz)
+        }
+    }
+
+    //get updated Jotz
+    private fun getUpdatedJotzEntry(
+        jotzId: Int,
+        jotzTitle: String,
+        jotzBody: String
+    ): Jotz {
+        return Jotz(
+            id = jotzId,
+            jotzTitle = jotzTitle,
+            jotzBody = jotzBody
+        )
+    }
+
     //add the new jotz to db using the insert method
-    fun addNewJotz(jotzTitle: String, jotzBody: String){
+    fun addNewJotz(jotzTitle: String, jotzBody: String) {
         val newJotz = getNewJotzEntry(jotzTitle, jotzBody)
         insertJotz(newJotz)
     }
 
     //check if entry is valid(not blank)
-    fun isJotzEntryValid(jotzTitle: String, jotzBody: String) : Boolean{
-        if (jotzTitle.isBlank() || jotzBody.isBlank()){
+    fun isJotzEntryValid(jotzTitle: String, jotzBody: String): Boolean {
+        if (jotzTitle.isBlank() || jotzBody.isBlank()) {
             return false
         }
         return true
     }
 
     //retrieve Jotz item using id
-    fun retrieveJotz(id: Int) : LiveData<Jotz>{
-        return  jotzDao.getItem(id).asLiveData()
+    fun retrieveJotz(id: Int): LiveData<Jotz> {
+        return jotzDao.getItem(id).asLiveData()
     }
 
     //delete Jotz item using id
@@ -49,6 +68,17 @@ class JotzViewModel(private val jotzDao: JotzDao) : ViewModel() {
             jotzDao.delete(jotz)
         }
     }
+
+    //update the jotz entry
+    fun updateJotzEntry(
+        jotzId: Int,
+        jotzTitle: String,
+        jotzBody: String
+    ) {
+        val updatedJotz = getUpdatedJotzEntry(jotzId, jotzTitle, jotzBody)
+        updateJotz(updatedJotz)
+    }
+
 
 }
 
