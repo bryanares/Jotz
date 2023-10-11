ccdbcb2265f85a8821ab13693a9b5690afbaadcf
bryanares <bryanares12@gmail.com>
2023-10-11 12:50:29 +0300

    update imports

--- app/src/main/java/com/brian/jotz/JotzApplication.kt
+++ app/src/main/java/com/brian/jotz/JotzApplication.kt
@@ -1,7 +1,7 @@
 package com.brian.jotz
 
 import android.app.Application
-import com.brian.jotz.data.JotzRoomDatabase
+import com.brian.jotz.data.database.JotzRoomDatabase
 
 class JotzApplication : Application() {
     val database: JotzRoomDatabase by lazy { JotzRoomDatabase.getDatabase(this) }
