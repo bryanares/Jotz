ae72b1baec77602677f2feae8fab19575e7a9cac
bryanares <bryanares12@gmail.com>
2023-10-11 12:42:38 +0300

    Hilt Module

--- app/src/main/java/com/brian/jotz/data/di/modules/JotzModule.kt
+++ app/src/main/java/com/brian/jotz/data/di/modules/JotzModule.kt
@@ -1,4 +1,18 @@
 package com.brian.jotz.data.di.modules
 
-class JotzModule {
+import androidx.room.Dao
+import com.brian.jotz.data.database.dao.JotzDao
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.internal.managers.ApplicationComponentManager
+
+@Module
+@InstallIn(ApplicationComponentManager::class)
+object JotzModule {
+
+    @Provides
+    fun provideDao (dao : JotzDao) : JotzDao {
+        return dao
+    }
 }
