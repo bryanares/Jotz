9b234209afc5be7f06a9b98333d66f3a1e552c05
bryanares <bryanares12@gmail.com>
2023-11-16 11:39:06 +0300

    provide database

--- app/src/main/java/com/brian/jotz/data/di/modules/JotzModule.kt
+++ app/src/main/java/com/brian/jotz/data/di/modules/JotzModule.kt
@@ -1,18 +1,31 @@
 package com.brian.jotz.data.di.modules
 
-import androidx.room.Dao
+import android.app.Application
+import androidx.room.Room
+import com.brian.jotz.data.database.JotzRoomDatabase
 import com.brian.jotz.data.database.dao.JotzDao
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
-import dagger.hilt.android.internal.managers.ApplicationComponentManager
+import dagger.hilt.components.SingletonComponent
 
 @Module
-@InstallIn(ApplicationComponentManager::class)
+@InstallIn(SingletonComponent::class)
 object JotzModule {
 
     @Provides
-    fun provideDao (dao : JotzDao) : JotzDao {
-        return dao
+    fun provideJotzDatabase(application: Application): JotzRoomDatabase {
+        return Room.databaseBuilder(
+            application,
+            JotzRoomDatabase::class.java, "database-name"
+        )
+            .fallbackToDestructiveMigration()
+            .build()
+
+    }
+
+    @Provides
+    fun provideDao(db: JotzRoomDatabase): JotzDao {
+        return db.jotzDao()
     }
 }
