a096a56ce7f8f4e97b26dd23784c5baf8cd1a470
bryanares <bryanares12@gmail.com>
2023-11-17 14:02:32 +0300

    collect user detail and create user

--- app/src/main/java/com/brian/jotz/features/signup/SignupFragment.kt
+++ app/src/main/java/com/brian/jotz/features/signup/SignupFragment.kt
@@ -1,33 +1,33 @@
 package com.brian.jotz.features.signup
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import androidx.navigation.fragment.findNavController
 import com.brian.jotz.R
+import com.brian.jotz.databinding.FragmentSignupBinding
+import com.brian.jotz.domain.viewmodels.JotzViewModel
+import com.brian.jotz.features.auth.domain.viewmodel.AuthViewModel
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [SignupFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
+@AndroidEntryPoint
 class SignupFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private lateinit var _binding : FragmentSignupBinding
+    private val binding get() = _binding
+    private val authViewModel: AuthViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+
     }
 
     override fun onCreateView(
@@ -35,26 +35,44 @@ class SignupFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_signup, container, false)
+        _binding = FragmentSignupBinding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment SignupFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            SignupFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        //call signup method from viewmodel to signup user, and navigate to home fragment if successful
+        binding.signupBt.setOnClickListener {
+            authViewModel.signup(
+                binding.emailSignup.editText?.text.toString(),
+                binding.passwordSignup.editText?.text.toString(),
+                binding.confirmPasswordSignup.editText?.text.toString(),
+                binding.nameSignup.editText?.text.toString()
+            )
+        }
+        collectLatestStates()
+    }
+
+    //
+    private fun collectLatestStates() {
+        viewLifecycleOwner.lifecycleScope.launch {
+            viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.RESUMED) {
+                authViewModel.authUiState.collectLatest { state ->
+                    binding.signupBt.isEnabled = !state.isLoading
+                    if (state.isSuccessful) {
+                        findNavController().navigate(
+                            SignupFragmentDirections.actionSignupFragmentToJotzListFragment(
+                                state.userId!!
+                            )
+                        )
+                    }
+                    state.error?.let {
+                        Toast.makeText(requireContext(), it, Toast.LENGTH_SHORT).show()
+                    }
+                    authViewModel.resetState()
                 }
             }
+        }
     }
 }
