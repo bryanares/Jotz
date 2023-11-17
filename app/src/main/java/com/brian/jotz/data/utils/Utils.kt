6fefd6a2ae6d637c2468bf81195bf26147e6976a
bryanares <bryanares12@gmail.com>
2023-11-17 13:38:41 +0300

    api responses

--- app/src/main/java/com/brian/jotz/data/utils/Utils.kt
+++ app/src/main/java/com/brian/jotz/data/utils/Utils.kt
@@ -1,4 +1,8 @@
 package com.brian.jotz.data.utils
 
-class Utils {
+//created a sealed Rezults class to handle the response from the api
+sealed class Rezults<T> {
+    data class Success<T>(val data: T) : Rezults<T>()
+    data class Error<T>(val message: String? = null, val exception: Exception? = null) : Rezults<T>()
+
 }
