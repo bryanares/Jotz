0c9a28ea688ae32e7118305bc8187f54b5afe4b5
bryanares <bryanares12@gmail.com>
2023-11-17 13:32:56 +0300

    create user model

--- app/src/main/java/com/brian/jotz/data/local/User.kt
+++ app/src/main/java/com/brian/jotz/data/local/User.kt
@@ -1,4 +1,8 @@
 package com.brian.jotz.data.local
 
-class User {
-}
+data class User (
+    val id : String? =null,
+    val name : String? =null,
+    val email : String? =null,
+    val createdTime : Long? =null,
+)
