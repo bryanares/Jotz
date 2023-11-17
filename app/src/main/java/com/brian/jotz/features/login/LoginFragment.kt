119e2fbee448c420048ea28278429dae044b3581
bryanares <bryanares12@gmail.com>
2023-11-17 13:46:15 +0300

    login and update ui

--- app/src/main/java/com/brian/jotz/features/login/LoginFragment.kt
+++ app/src/main/java/com/brian/jotz/features/login/LoginFragment.kt
@@ -1,33 +1,33 @@
 package com.brian.jotz.features.login
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import androidx.navigation.fragment.findNavController
 import com.brian.jotz.R
+import com.brian.jotz.databinding.FragmentLoginBinding
+import com.brian.jotz.features.auth.domain.viewmodel.AuthViewModel
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [LoginFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
+@AndroidEntryPoint
 class LoginFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+
+
+    private lateinit var _binding: FragmentLoginBinding
+    private val binding get() = _binding
+    private val authViewModel: AuthViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
     }
 
     override fun onCreateView(
@@ -35,26 +35,47 @@ class LoginFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false)
+        _binding = FragmentLoginBinding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment LoginFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            LoginFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+    //call login method from ViewModel to login user, and navigate to home fragment if successful
+    // if user doesn't have an account and wants to register, navigate to registration fragment
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        collectLatestStates()
+        binding.loginBt.setOnClickListener {
+            authViewModel.login(
+                binding.emailLogin.editText?.text.toString(),
+                binding.passwordLogin.editText?.text.toString()
+            )
+        }
+        binding.signupTextView.setOnClickListener {
+            findNavController().navigate(R.id.action_loginFragment_to_signupFragment)
+        }
+    }
+
+    //collect latest states, and navigate to home fragment if successful
+    private fun collectLatestStates() {
+        viewLifecycleOwner.lifecycleScope.launch {
+            viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.RESUMED) {
+                authViewModel.authUiState.collectLatest { state ->
+                    binding.loginBt.isEnabled = !state.isLoading
+                    if (state.isSuccessful) {
+                        findNavController().navigate(
+                            LoginFragmentDirections.actionLoginFragmentToJotzListFragment(
+                                state.userId!!
+                            )
+                        )
+                    }
+                    if (state.error != null) {
+                        Toast.makeText(requireContext(), state.error, Toast.LENGTH_SHORT).show()
+                    }
+                    authViewModel.resetState()
                 }
             }
+        }
     }
+
 }
