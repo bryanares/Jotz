eb8ef262f54dd800dd9cea72377e336ad084bf0d
bryanares <bryanares12@gmail.com>
2023-11-17 13:35:19 +0300

    define and implement repository

--- app/src/main/java/com/brian/jotz/data/repository/MainRepository.kt
+++ app/src/main/java/com/brian/jotz/data/repository/MainRepository.kt
@@ -1,4 +1,13 @@
 package com.brian.jotz.data.repository
 
-interface Repository {
+import com.brian.jotz.data.local.User
+import com.brian.jotz.data.utils.Rezults
+import kotlinx.coroutines.flow.Flow
+
+interface MainRepository {
+
+    suspend fun signIn(email: String, password: String): Flow<Rezults<User>>
+
+    suspend fun signUp(email: String, password: String, name: String): Flow<Rezults<User>>
+
 }
--- app/src/main/java/com/brian/jotz/data/repository/MainRepositoryImpl.kt
+++ app/src/main/java/com/brian/jotz/data/repository/MainRepositoryImpl.kt
@@ -1,4 +1,78 @@
 package com.brian.jotz.data.repository
 
-class MainRepositoryImpl {
+import com.brian.jotz.data.local.User
+import com.brian.jotz.data.utils.Rezults
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import kotlinx.coroutines.cancel
+import kotlinx.coroutines.channels.awaitClose
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.callbackFlow
+import javax.inject.Inject
+
+
+class MainRepositoryImpl @Inject constructor(
+    private val firebaseAuth: FirebaseAuth,
+    private val firebaseFirestore: FirebaseFirestore
+) : MainRepository {
+
+    //sign in functionality, return user, or error
+    override suspend fun signIn(email: String, password: String): Flow<Rezults<User>> {
+        return callbackFlow {
+            firebaseAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener { task ->
+                    if (task.isSuccessful) {
+                        this.trySend(
+                            Rezults.Success(
+                                User(
+                                    firebaseAuth.currentUser!!.uid
+                                )
+                            )
+                        ).isSuccess
+
+
+                    } else {
+                        this.trySend(
+                            Rezults.Error(
+                                "",
+                                task.exception!!
+                            )
+                        ).isSuccess
+                    }
+                }
+            awaitClose { this.cancel() }
+        }
+    }
+
+    //sign up functionality, create user, add user to database, and return user, or error
+    override suspend fun signUp(
+        email: String,
+        password: String,
+        name: String
+    ): Flow<Rezults<User>> {
+        return callbackFlow {
+            firebaseAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener { task ->
+                    if (task.isSuccessful) {
+                        this.trySend(
+                            Rezults.Success(
+                                User(
+                                    firebaseAuth.currentUser!!.uid
+                                )
+                            )
+                        ).isSuccess
+
+                    } else {
+                        this.trySend(
+                            Rezults.Error(
+                                "",
+                                task.exception!!
+                            )
+                        ).isSuccess
+                    }
+                }
+            awaitClose { this.cancel() }
+        }
+    }
+
 }
