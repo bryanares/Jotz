bc62b968b4f42e4dd239ffe57a4e35ada9072387
bryanares <bryanares12@gmail.com>
2024-01-04 14:09:17 +0300

    create model for firestore document

--- app/src/main/java/com/brian/jotz/data/local/JotItem.kt
+++ app/src/main/java/com/brian/jotz/data/local/JotItem.kt
@@ -1,4 +1,12 @@
 package com.brian.jotz.data.local
 
-class JotItem {
-}
+data class JotItem (
+    val id: String? = null,
+    val userId: String? = null,
+    val date: Long? = null,
+    val title: String? = null,
+    val category: String ?= null,
+    val body: String? = null,
+    var createdTime: Long? = null,
+    var updatedTime: Long? = null,
+)
