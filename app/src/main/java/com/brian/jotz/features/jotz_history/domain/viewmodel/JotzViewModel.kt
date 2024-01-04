b575e37d6177263536c0b952360ec5c85745e1a4
bryanares <bryanares12@gmail.com>
2024-01-04 15:46:11 +0300

    implement firestore methods

--- app/src/main/java/com/brian/jotz/features/jotz_history/domain/viewmodel/JotzViewModel.kt
+++ app/src/main/java/com/brian/jotz/features/jotz_history/domain/viewmodel/JotzViewModel.kt
@@ -1,11 +1,13 @@
-package com.brian.jotz.domain.viewmodels
+package com.brian.jotz.features.jotz_history.domain.viewmodel
 
 import androidx.lifecycle.*
 import com.brian.jotz.data.database.dao.JotzDao
 import com.brian.jotz.data.database.entities.Jotz
+import com.brian.jotz.data.local.JotItem
 import com.brian.jotz.data.repository.MainRepository
 import com.brian.jotz.data.utils.Rezults
 import com.brian.jotz.features.auth.domain.model.AuthUiState
+import com.brian.jotz.features.jotz_history.domain.model.JotItemUiState
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -18,87 +20,236 @@ import javax.inject.Inject
 @HiltViewModel
 class JotzViewModel
 @Inject constructor
-    (private val jotzDao: JotzDao,
-            private val repository: MainRepository
-    ) : ViewModel() {
-
-    val allJotz: LiveData<List<Jotz>> = jotzDao.getItems().asLiveData()
-    private val _authUiState = MutableStateFlow(AuthUiState())
-    val authUiState = _authUiState.asStateFlow()
+    (
+    private val jotzDao: JotzDao,
+    private val repository: MainRepository
+) : ViewModel() {
+    private val _jotItemUiState = MutableStateFlow(JotItemUiState())
+    val jotItemUiState = _jotItemUiState.asStateFlow()
 
     fun resetState() {
-        _authUiState.value = AuthUiState()
+        _jotItemUiState.update { JotItemUiState() }
     }
 
-    //call dao method to insert data to db
-    private fun insertJotz(jotz: Jotz) {
-        viewModelScope.launch {
-            jotzDao.insert(jotz)
+    fun addJotItem(userId: String, date: Long, title: String, body: String) {
+        viewModelScope.launch(Dispatchers.IO) {
+
+            repository.addOrUpdateJotz(
+                userId,
+                null,
+                JotItem(date = date, title = title, body = body)
+            ).collectLatest { rezult ->
+                when (rezult) {
+                    is Rezults.Success -> {
+                        _jotItemUiState.update {
+                            JotItemUiState(
+                                isLoading = false,
+                                isSuccessful = true,
+                                createdJotItem = rezult.data
+                            )
+                        }
+                    }
+
+                    is Rezults.Error -> {
+                        _jotItemUiState.update {
+                            JotItemUiState(
+                                isLoading = false,
+                                isSuccessful = false,
+                                error = rezult.exception?.message
+                            )
+                        }
+                    }
+                }
+            }
         }
     }
 
-    //get new jotz details and define the item
-    private fun getNewJotzEntry(jotzTitle: String, jotzBody: String): Jotz {
-        return Jotz(
-            jotzTitle = jotzTitle.toString(),
-            jotzBody = jotzBody.toString()
-        )
-    }
-
-    private fun updateJotz(jotz: Jotz) {
-        viewModelScope.launch {
-            jotzDao.update(jotz)
-        }
-    }
-
-    //get updated Jotz
-    private fun getUpdatedJotzEntry(
-        jotzId: Int,
-        jotzTitle: String,
-        jotzBody: String
-    ): Jotz {
-        return Jotz(
-            id = jotzId,
-            jotzTitle = jotzTitle,
-            jotzBody = jotzBody
-        )
-    }
-
-    //add the new jotz to db using the insert method
-    fun addNewJotz(jotzTitle: String, jotzBody: String) {
-        val newJotz = getNewJotzEntry(jotzTitle, jotzBody)
-        insertJotz(newJotz)
-    }
-
-    //check if entry is valid(not blank)
-    fun isJotzEntryValid(jotzTitle: String, jotzBody: String): Boolean {
-        if (jotzTitle.isBlank() || jotzBody.isBlank()) {
-            return false
-        }
-        return true
-    }
-
-    //retrieve Jotz item using id
-    fun retrieveJotz(id: Int): LiveData<Jotz> {
-        return jotzDao.getItem(id).asLiveData()
-    }
-
-    //delete Jotz item using id
-    fun deleteJotz(jotz: Jotz) {
-        viewModelScope.launch {
-            jotzDao.delete(jotz)
-        }
-    }
-
-    //update the jotz entry
-    fun updateJotzEntry(
-        jotzId: Int,
-        jotzTitle: String,
-        jotzBody: String
+    fun updateJotItem(
+        userId: String,
+        jotItemId: String,
+        date: Long? = null,
+        title: String? = null,
+        body: String? = null
     ) {
-        val updatedJotz = getUpdatedJotzEntry(jotzId, jotzTitle, jotzBody)
-        updateJotz(updatedJotz)
+        viewModelScope.launch(Dispatchers.IO) {
+            val oldJotItem = _jotItemUiState.value.singleJotItem ?: return@launch
+            val newJotItem = oldJotItem.copy(
+                date = date ?: oldJotItem.date,
+                title = title ?: oldJotItem.title,
+                body = body ?: oldJotItem.body
+            )
+
+            repository.addOrUpdateJotz(
+                userId,
+                jotItemId,
+                newJotItem
+            )
+                .collectLatest { rezult ->
+                    when (rezult){
+                        is Rezults.Success -> {
+                            _jotItemUiState.update {
+                                JotItemUiState(
+                                    isLoading = false,
+                                    isSuccessful = true,
+                                    updatedJotItem = rezult.data
+                                )
+                            }
+                        }
+                        is Rezults.Error ->{
+                            _jotItemUiState.update {
+                                JotItemUiState(
+                                    isLoading = false,
+                                    isSuccessful = false,
+                                    error = rezult.exception?.message
+                                )
+                            }
+                        }
+                    }
+                }
+        }
     }
+
+    fun getSingleJotItem(
+        userId : String,
+        jotItemId: String
+    ){
+        viewModelScope.launch(Dispatchers.IO){
+
+            repository.getSingleJotItem(
+                userId,
+                jotItemId
+            ).collectLatest { rezult ->
+                when (rezult){
+                    is Rezults.Success -> {
+                        _jotItemUiState.update {
+                            JotItemUiState(
+                                isLoading = false,
+                                isSuccessful = true,
+                                singleJotItem = rezult.data
+                            )
+                        }
+                    }
+
+                    is Rezults.Error -> {
+                        _jotItemUiState.update {
+                            JotItemUiState(
+                                isLoading = false,
+                                isSuccessful = false,
+                                error = rezult.exception?.message
+                            )
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    fun getAllJotItems(userId: String){
+        viewModelScope.launch (Dispatchers.IO){
+
+            repository.getAllJotItems(userId).collectLatest { rezult ->
+
+                when (rezult) {
+                    is Rezults.Success -> {
+                        _jotItemUiState.update {
+                            JotItemUiState(
+                                isLoading = false,
+                                isSuccessful = true,
+                                jotItemList = rezult.data
+                            )
+                        }
+                    }
+                    is Rezults.Error -> {
+                        _jotItemUiState.update {
+                            JotItemUiState(
+                                isLoading = false,
+                                isSuccessful = false,
+                                error = rezult.exception?.message
+                            )
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+//    val allJotz: LiveData<List<Jotz>> = jotzDao.getItems().asLiveData()
+//    private val _authUiState = MutableStateFlow(AuthUiState())
+//    val authUiState = _authUiState.asStateFlow()
+
+//    fun resetState() {
+//        _authUiState.value = AuthUiState()
+//    }
+
+//    //call dao method to insert data to db
+//    private fun insertJotz(jotz: Jotz) {
+//        viewModelScope.launch {
+//            jotzDao.insert(jotz)
+//        }
+//    }
+//
+//    //get new jotz details and define the item
+//    private fun getNewJotzEntry(jotzTitle: String, jotzBody: String): Jotz {
+//        return Jotz(
+//            jotzTitle = jotzTitle.toString(),
+//            jotzBody = jotzBody.toString()
+//        )
+//    }
+//
+//    private fun updateJotz(jotz: Jotz) {
+//        viewModelScope.launch {
+//            jotzDao.update(jotz)
+//        }
+//    }
+//
+//    //get updated Jotz
+//    private fun getUpdatedJotzEntry(
+//        jotzId: Int,
+//        jotzTitle: String,
+//        jotzBody: String
+//    ): Jotz {
+//        return Jotz(
+//            id = jotzId,
+//            jotzTitle = jotzTitle,
+//            jotzBody = jotzBody
+//        )
+//    }
+//
+//    //add the new jotz to db using the insert method
+//    fun addNewJotz(jotzTitle: String, jotzBody: String) {
+//        val newJotz = getNewJotzEntry(jotzTitle, jotzBody)
+//        insertJotz(newJotz)
+//    }
+//
+//    //check if entry is valid(not blank)
+//    fun isJotzEntryValid(jotzTitle: String, jotzBody: String): Boolean {
+//        if (jotzTitle.isBlank() || jotzBody.isBlank()) {
+//            return false
+//        }
+//        return true
+//    }
+//
+//    //retrieve Jotz item using id
+//    fun retrieveJotz(id: Int): LiveData<Jotz> {
+//        return jotzDao.getItem(id).asLiveData()
+//    }
+//
+//    //delete Jotz item using id
+//    fun deleteJotz(jotz: Jotz) {
+//        viewModelScope.launch {
+//            jotzDao.delete(jotz)
+//        }
+//    }
+//
+//    //update the jotz entry
+//    fun updateJotzEntry(
+//        jotzId: Int,
+//        jotzTitle: String,
+//        jotzBody: String
+//    ) {
+//        val updatedJotz = getUpdatedJotzEntry(jotzId, jotzTitle, jotzBody)
+//        updateJotz(updatedJotz)
+//    }
 
 
 }
