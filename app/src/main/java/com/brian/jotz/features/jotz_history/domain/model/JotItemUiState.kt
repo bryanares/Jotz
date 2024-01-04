f4d6cc20b9c4d49a7daa2bf0cf3294774b445524
bryanares <bryanares12@gmail.com>
2024-01-04 15:45:28 +0300

    create UI states for Jot Item

--- app/src/main/java/com/brian/jotz/features/jotz_history/domain/model/JotItemUiState.kt
+++ app/src/main/java/com/brian/jotz/features/jotz_history/domain/model/JotItemUiState.kt
@@ -1,4 +1,13 @@
 package com.brian.jotz.features.jotz_history.domain.model
 
-class JotItemUiState {
-}
+import com.brian.jotz.data.local.JotItem
+
+data class JotItemUiState (
+    val isLoading: Boolean = false,
+    val isSuccessful: Boolean = false,
+    val error: String? = null,
+    val updatedJotItem: JotItem? = null,
+    val createdJotItem: JotItem? = null,
+    val singleJotItem: JotItem? = null,
+    val jotItemList: List<JotItem>? = null,
+)
