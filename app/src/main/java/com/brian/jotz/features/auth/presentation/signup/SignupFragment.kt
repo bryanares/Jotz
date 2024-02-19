e53670379cbfd4b444528b44d39207c7b8a125ee
bryanares <bryanares12@gmail.com>
2024-02-19 12:49:35 +0300

    optimize code

--- app/src/main/java/com/brian/jotz/features/auth/presentation/signup/SignupFragment.kt
+++ app/src/main/java/com/brian/jotz/features/auth/presentation/signup/SignupFragment.kt
@@ -24,10 +24,6 @@ class SignupFragment : Fragment() {
     //    private val binding get() = _binding!!
     private val authViewModel: AuthViewModel by viewModels()
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
